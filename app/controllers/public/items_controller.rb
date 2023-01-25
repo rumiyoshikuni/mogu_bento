8f6327b37757f88c3135b158449d59ea28c94f61
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2023-01-25 12:35:31 +0000

    ページネーションレイアウト

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -6,9 +6,9 @@ class Public::ItemsController < ApplicationController
     @genres = Genre.all
     if params[:genre_id]
       @genre = Genre.find(params[:genre_id])
-      @items = @genre.items.where(is_active: 1).page(params[:page]).per(8)
+      @items = @genre.items.where(is_active: 1)
     else
-      @items = Item.where(is_active: 1).page(params[:page]).per(8)
+      @items = Item.where(is_active: 1)
     end
   end
   # メニュー詳細
