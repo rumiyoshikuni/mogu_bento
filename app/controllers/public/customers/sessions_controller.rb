9d17f818abff3dd70bef0b951fb1467819a496e1
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2023-01-26 11:38:46 +0000

    ゲストユーザー表記日本語化

--- app/controllers/public/customers/sessions_controller.rb
+++ app/controllers/public/customers/sessions_controller.rb
@@ -3,6 +3,6 @@ class Public::Customers::SessionsController < ApplicationController
   def guest_sign_in
     customer = Customer.guest
     sign_in customer
-    redirect_to items_path, notice: 'guestcustomerでログインしました。'
+    redirect_to items_path, notice: 'ゲストでログインしました。'
   end
 end
