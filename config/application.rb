40ab43ba9f6c8db35c2f8d228bdeebb49ace20e4
rumiyoshikuni <115619656+rumiyoshikuni@users.noreply.github.com>
2023-01-27 09:07:45 +0000

    application.rb追記

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module MoguBento
     config.i18n.load_path += Dir[Rails.root.join('config/locales/*.yml').to_s]
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag }
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
